970a35878b6ed225da38c7a8050bf272a74f6ff9
billalxcode <billal.xcode@gmail.com>
2023-09-19 09:37:27 +0700

    menambahkan tenaga kependidikan

---
+++ app/Http/Controllers/TenagaKependidikanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TenagaKependidikan;
+use App\Http\Requests\StoreTenagaKependidikanRequest;
+use App\Http\Requests\UpdateTenagaKependidikanRequest;
+
+class TenagaKependidikanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTenagaKependidikanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TenagaKependidikan $tenagaKependidikan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TenagaKependidikan $tenagaKependidikan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTenagaKependidikanRequest $request, TenagaKependidikan $tenagaKependidikan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TenagaKependidikan $tenagaKependidikan)
+    {
+        //
+    }
+}
