9317111f252839bcbad5809427deaf725e526ac9
billalxcode <billal.xcode@gmail.com>
2023-09-22 09:47:43 +0700

    remove commented element

--- resources/views/layout/operator/navbar-menu.blade.php
+++ resources/views/layout/operator/navbar-menu.blade.php
@@ -100,9 +100,6 @@
                         </div>
                     </li>
                 </ul>
-                <!-- <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
-                    <button class="btn btn-primary">Support</button>
-                </div> -->
             </div>
         </div>
     </div>
