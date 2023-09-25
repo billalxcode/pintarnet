05a975109183cbc0cf47cea73a9ff879a4f64c7b
billalxcode <billal.xcode@gmail.com>
2023-09-26 06:53:14 +0700

    fix typo

--- resources/views/layout/ruangan/data-information.blade.php
+++ resources/views/layout/ruangan/data-information.blade.php
@@ -37,7 +37,7 @@
                         </div>
                         <div class="col">
                             <div class="font-weight-medium">
-                                {{ $ruangan_total_sakit        ddz }} Sakit
+                                {{ $ruangan_total_sakit }} Sakit
                             </div>
                         </div>
                     </div>
@@ -61,7 +61,7 @@
                         </div>
                         <div class="col">
                             <div class="font-weight-medium">
-                                {{ $total_tenaga_pendidik }} Tenaga Pendidik
+                                {{ $ruangan_total_izin }} Izin
                             </div>
                         </div>
                     </div>
@@ -73,7 +73,7 @@
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col-auto">
-                            <span class="bg-facebook text-white avatar">
+                            <span class="bg-danger text-white avatar">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                     <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
@@ -85,7 +85,7 @@
                         </div>
                         <div class="col">
                             <div class="font-weight-medium">
-                                {{ $total_tenaga_kependidikan }} Tenaga Kependidikan
+                                {{ $ruangan_total_alpha }} Alpha
                             </div>
                         </div>
                     </div>
