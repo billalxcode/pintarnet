09c4c41481a8c845921d72642fe1175812bafbc3
billalxcode <billal.xcode@gmail.com>
2023-10-24 16:18:58 +0700

    fix ruangan detail

--- resources/views/operator/ruangan/detail.blade.php
+++ resources/views/operator/ruangan/detail.blade.php
@@ -1,128 +1,115 @@
 @extends('layout.operator.app')
 
 @section('content')
-    <!-- Page header -->
-    <div class="page-header d-print-none">
-        <div class="container-xl">
-            <div class="row g-2 align-items-center">
-                <div class="col">
-                    <div class="page-pretitle">
-                        Ruangan / Detail
-                    </div>
-                    <h2 class="page-title">
-                        {{ $ruanganx->nama ?? 'Tidak diketahui' }}
-                    </h2>
+<!-- Page header -->
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <div class="page-pretitle">
+                    Ruangan / Detail
                 </div>
+                <h2 class="page-title">
+                    {{ $ruanganx->nama ?? 'Tidak diketahui' }}
+                </h2>
             </div>
         </div>
     </div>
-    <!-- Page body -->
-    <div class="page-body">
-        <div class="container-xl">
-            <div class="row row-deck row-cards">
-                @include('partials.alert')
+</div>
+<!-- Page body -->
+<div class="page-body">
+    <div class="container-xl">
+        <div class="row row-deck row-cards">
+            @include('partials.alert')
 
-                <div class="col-12">
-                    <div class="row row-cards">
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card card-sm">
-                                <div class="card-body">
-                                    <div class="row align-items-center">
-                                        <div class="col-auto">
-                                            <span class="bg-danger text-white avatar">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="icon icon-tabler icon-tabler-school" width="44"
-                                                    height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff"
-                                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
-                                                    <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
-                                                </svg>
-                                            </span>
-                                        </div>
-                                        <div class="col">
-                                            <div class="font-weight-medium">
-                                                {{ $ruanganx->kehadiran->jumlah_bolos ?? '0' }} Bolos
-                                            </div>
+            <div class="col-12">
+                <div class="row row-cards">
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card card-sm">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-auto">
+                                        <span class="bg-danger text-white avatar">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-school" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+                                                <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <div class="font-weight-medium">
+                                            {{ $ruanganx->kehadiran->jumlah_bolos ?? '0' }} Bolos
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card card-sm">
-                                <div class="card-body">
-                                    <div class="row align-items-center">
-                                        <div class="col-auto">
-                                            <span class="bg-primary text-white avatar">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="icon icon-tabler icon-tabler-school" width="44"
-                                                    height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff"
-                                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
-                                                    <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
-                                                </svg>
-                                            </span>
-                                        </div>
-                                        <div class="col">
-                                            <div class="font-weight-medium">
-                                                {{ $ruanganx->kehadiran->jumlah_sakit ?? '0' }} Sakit
-                                            </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card card-sm">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-auto">
+                                        <span class="bg-primary text-white avatar">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-school" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+                                                <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <div class="font-weight-medium">
+                                            {{ $ruanganx->kehadiran->jumlah_sakit ?? '0' }} Sakit
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card card-sm">
-                                <div class="card-body">
-                                    <div class="row align-items-center">
-                                        <div class="col-auto">
-                                            <span class="bg-twitter text-white avatar">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="icon icon-tabler icon-tabler-users" width="44" height="44"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none"
-                                                    stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-                                                    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                                                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                                    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-                                                </svg>
-                                            </span>
-                                        </div>
-                                        <div class="col">
-                                            <div class="font-weight-medium">
-                                                {{ $ruanganx->kehadiran->jumlah_izin ?? '0' }} Izin
-                                            </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card card-sm">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-auto">
+                                        <span class="bg-twitter text-white avatar">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                                                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                                                <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <div class="font-weight-medium">
+                                            {{ $ruanganx->kehadiran->jumlah_izin ?? '0' }} Izin
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card card-sm">
-                                <div class="card-body">
-                                    <div class="row align-items-center">
-                                        <div class="col-auto">
-                                            <span class="bg-danger text-white avatar">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="icon icon-tabler icon-tabler-users" width="44" height="44"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none"
-                                                    stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-                                                    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                                                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                                    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-                                                </svg>
-                                            </span>
-                                        </div>
-                                        <div class="col">
-                                            <div class="font-weight-medium">
-                                                {{ $ruanganx->kehadiran->jumlah_alpha ?? '0' }} Alpha
-                                            </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card card-sm">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-auto">
+                                        <span class="bg-danger text-white avatar">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                                                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                                                <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <div class="font-weight-medium">
+                                            {{ $ruanganx->kehadiran->jumlah_alpha ?? '0' }} Alpha
                                         </div>
                                     </div>
                                 </div>
@@ -130,23 +117,70 @@
                         </div>
                     </div>
                 </div>
+            </div>
 
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Data Siswa</h3>
+                    </div>
+                    <div class="card-body border-bottom py-3">
+                        <div class="table-responsive">
+                            <table class="table card-table table-vcenter text-nowrap datatable">
+                                <thead>
+                                    <tr>
+                                        <th>NIS</th>
+                                        <th>NISN</th>
+                                        <th>Nama Lengkap</th>
+                                        <th>Created</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($ruanganx->siswas as $data)
+                                    <tr>
+                                        <td>
+                                            <span class="text-muted">{{ $data->nis ?? 'Belum diisi' }}</span>
+                                        </td>
+                                        <td>
+                                            <span class="text-muted">{{ $data->nisn ?? 'Belum diisi' }}</span>
+                                        </td>
+                                        <td>{{ $data->nama }}</td>
+                                        <td>{{ $data->created_at }}</td>
+                                        <td class="d-flex gap-1">
+                                            <form action="{{ route('operator.siswa.destroy', $data->id) }}" method="post" id="{{ 'siswa-' . $data->id }}">
+                                                @csrf
+                                                @method('DELETE')
+
+                                                <!-- <input type="hidden" name="id" value="{{ $data->id }}"> -->
+                                                <button class="btn btn-danger" type="submit">Delete</button>
+                                            </form>
+                                            <a href="{{ route('operator.siswa.show', $data->id) }}" class="btn btn-info">Detail</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push('modals')
 @endpush
 
 @push('scripts')
-    <script>
-        $("#btn-save").on("click", function() {
-            $("#form-save").submit()
-        })
+<script>
+    $("#btn-save").on("click", function() {
+        $("#form-save").submit()
+    })
 
-        $(document).ready(function() {
-            $(".datatable").DataTable()
-        })
-    </script>
+    $(document).ready(function() {
+        $(".datatable").DataTable()
+    })
+</script>
 @endpush
