facc82c9d2f52fd3dd7995a0e3c1e21188d5e309
billalxcode <billal.xcode@gmail.com>
2023-10-26 11:10:53 +0700

    fix add new data in setting token

--- resources/views/operator/setting/token.blade.php
+++ resources/views/operator/setting/token.blade.php
@@ -29,7 +29,7 @@
                             New Token
                         </a>
                         <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
-                            data-bs-target="#modal-report" aria-label="Create new report">
+                            data-bs-target="#modal-new-data" aria-label="Create new report">
                             <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
