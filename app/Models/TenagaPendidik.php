a1f24905176d1f6544466bd9faa0a3f3c8e7d043
billalxcode <billal.xcode@gmail.com>
2023-10-26 15:09:05 +0700

    update tenaga pendidik model

--- app/Models/TenagaPendidik.php
+++ app/Models/TenagaPendidik.php
@@ -14,4 +14,8 @@ class TenagaPendidik extends Model
         'nip', 'nama', 'mapel', 'jk', 'alamat',
         'tempat_lahir', 'tanggal_lahir', 'mapel_id'
     ];
+
+    public function mapel() {
+        return $this->hasOne(MataPelajaran::class, 'id', 'mapel_id');
+    }
 }
