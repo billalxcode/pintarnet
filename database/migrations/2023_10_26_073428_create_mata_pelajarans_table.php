7f31d0239beba664b1c9a7434f279645e40db2fc
billalxcode <billal.xcode@gmail.com>
2023-10-26 15:24:35 +0700

    update mapel migration

--- database/migrations/2023_10_26_073428_create_mata_pelajarans_table.php
+++ database/migrations/2023_10_26_073428_create_mata_pelajarans_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('mata_pelajarans', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('keterangan');
             $table->timestamps();
         });
     }
