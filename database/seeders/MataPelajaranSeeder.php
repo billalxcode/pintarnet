2f4d47a42fd2704757b6e2895c359b5d19f795b3
billalxcode <billal.xcode@gmail.com>
2023-10-26 15:24:44 +0700

    update mapel seeder

--- database/seeders/MataPelajaranSeeder.php
+++ database/seeders/MataPelajaranSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class MataPelajaranSeeder extends Seeder
 {
@@ -12,6 +13,8 @@ class MataPelajaranSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $json = Storage::disk('public')->get('json/mapel.json');
+        
+        dd($json);
     }
 }
