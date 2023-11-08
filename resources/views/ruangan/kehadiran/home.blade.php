d4c298bf610b45a86b9dcdadd43a8abf8c957ef7
billalxcode <billal.xcode@gmail.com>
2023-11-08 13:47:11 +0700

    remove select2 style from kehadiran

--- resources/views/ruangan/kehadiran/home.blade.php
+++ resources/views/ruangan/kehadiran/home.blade.php
@@ -210,13 +210,7 @@
 </div>
 @endpush
 
-@push('styles')
-    <!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" /> -->
-@endpush
-
 @push('scripts')
-<!-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->
 <script>
     $("#btn-save").on("click", function() {
         const html = $(this).parent().parent().find("#form-save")
