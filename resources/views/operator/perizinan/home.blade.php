9a0444c1523db43305a06ae43b62701d79179c6d
Billal Fauzan <billal.xcode@gmail.com>
2023-11-09 19:09:47 +0700

    delete modal from perizinan

--- resources/views/operator/perizinan/home.blade.php
+++ resources/views/operator/perizinan/home.blade.php
@@ -118,43 +118,7 @@
 @endsection
 
 @push('modals')
-<div class="modal modal-blur fade" id="modal-new-data" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">New Data</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="{{ route('operator.ruangan.store') }}" method="post" id="form-save">
-                    @csrf
-                    <div class="mb-3">
-                        <label class="form-label">Nama ruangan</label>
-                        <input type="text" class="form-control" name="nama" placeholder="Nama Ruangan">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Keterangan</label>
-                        <input type="text" class="form-control" name="keterangan" placeholder="Keterangan">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
-                    Cancel
-                </a>
-                <a href="#" class="btn btn-primary ms-auto" id="btn-save">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M12 5l0 14" />
-                        <path d="M5 12l14 0" />
-                    </svg>
-                    Create new data
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
+
 @endpush
 
 @push('scripts')
